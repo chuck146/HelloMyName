3ed2b271e2550ea00c59900e1df062c5ce8471d9
sam98@njit.edu <sam98@njit.edu>
2020-06-02 02:22:18 -0400

    Feature: added a favorite ice cream.

---
+++ HelloMyNameIs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloMyNameIs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string fname = "Steven";
+            string lname = "Morales";
+            string icecream = "Strawberry";
+            Console.WriteLine("Hello my name is {0} {1}", fname, lname);
+            Console.WriteLine("My favorite ice cream is {0}.", icecream);
+
+        }
+    }
+}
